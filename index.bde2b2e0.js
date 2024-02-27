5de901957386657b0117e506974a260306b64b24
Misha Remeslo <misha.remeslol@gmail.com>
2024-02-27 21:01:12 +0200

    make build

---
+++ index.bde2b2e0.js
@@ -0,0 +1,3 @@
+var e=1;!function(l){var a,s=document.getElementsByClassName("gallery-slide"),t=document.getElementsByClassName("gallery__dot");for(l>=s.length&&(e=0),l<0&&(e=s.length-1),a=0;a<s.length;a++)s[a].style.display="none";for(a=0;a<s.length;a++)s[a].style.display=s[a].style.display.replace("active","none");for(a=0;a<t.length;a++)t[a].className=t[a].className.replace(" active","");s[e].style.display="block",s[e-1].style.display="block",t[e].className+=" active"}(1);//# sourceMappingURL=index.bde2b2e0.js.map
+
+//# sourceMappingURL=index.bde2b2e0.js.map
